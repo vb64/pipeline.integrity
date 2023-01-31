06502e8a2a88b1ef4ded0f0ef8b67b291014053c
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-01-31 20:10:26 +0400

    step

--- pipeline_danger/__init__.py
+++ pipeline_danger/__init__.py
@@ -1,1 +1,5 @@
 """Pipeline defects danger calculation."""
+
+
+class Error(Exception):
+    """Pipe dunger base error class."""
--- pipeline_danger/pipe.py
+++ pipeline_danger/pipe.py
@@ -1,8 +1,11 @@
 """Single pipe from the pipeline."""
+from . import Error as ErrorBase
+
+CIRCLE_MINUTES = 60.0
 
 
-class Error(Exception):
-    """Pipe dunger error."""
+class Error(ErrorBase):
+    """Pipe error."""
 
 
 class Pipe:
@@ -19,6 +22,30 @@ class Pipe:
     def add_metal_loss(self, start, length, orient_start, orient_length, depth):
         """Add and return metal loss defect at the pipe."""
         from .defect.metal_loss import Item
+
+        if (start + length) > self.length:
+            raise Error(
+              "Defect border outside pipe: start {} + length {} = {} > pipe length {}.".format(
+                start, length, start + length, self.length
+              )
+            )
+
+        if depth > self.wallthickness:
+            raise Error("Defect depth {} > pipe wall thickness {}.".format(depth, self.wallthickness))
+
+        if not (-CIRCLE_MINUTES <= orient_start <= CIRCLE_MINUTES):
+            raise Error(
+              "Defect start orientdtion {} must be from {} to {} angle minutes.".format(
+                orient_start, -CIRCLE_MINUTES, CIRCLE_MINUTES
+              )
+            )
+
+        if not (0 < orient_length <= CIRCLE_MINUTES):
+            raise Error(
+              "Defect orientdtion length {} must be from 0 to {} angle minutes.".format(
+                orient_length, CIRCLE_MINUTES
+              )
+            )
 
         self.metal_loss.append(Item(self, start, length, orient_start, orient_length, depth))
         return self.metal_loss[-1]

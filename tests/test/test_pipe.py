7c0fe1b844dbc569b0c2a107a3363a3ec09ccc2d
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-01-31 20:18:45 +0400

    coverage

--- tests/test/test_pipe.py
+++ tests/test/test_pipe.py
@@ -2,7 +2,7 @@
 
 make test T=test_pipe.py
 """
-# import pytest
+import pytest
 from . import TestBase
 
 
@@ -11,8 +11,26 @@ class TestsPipe(TestBase):
 
     def test_add_metal_loss(self):
         """Method add_metal_loss."""
+        from pipeline_danger import Error
         from pipeline_danger.defect import Type
+        from pipeline_danger.pipe import CIRCLE_MINUTES
 
         defect = self.pipe.add_metal_loss(10, 100, 10, 20, 5)
         assert len(self.pipe.metal_loss) == 1
         assert defect.type == Type.MetalLoss
+
+        with pytest.raises(Error) as err:
+            self.pipe.add_metal_loss(self.pipe.length, 1, 10, 20, 5)
+        assert 'Defect border outside pipe:' in str(err.value)
+
+        with pytest.raises(Error) as err:
+            self.pipe.add_metal_loss(10, 100, CIRCLE_MINUTES + 1, 20, 5)
+        assert 'Defect start orientdtion ' in str(err.value)
+
+        with pytest.raises(Error) as err:
+            self.pipe.add_metal_loss(10, 100, 10, CIRCLE_MINUTES + 1, 5)
+        assert 'Defect orientdtion length ' in str(err.value)
+
+        with pytest.raises(Error) as err:
+            self.pipe.add_metal_loss(10, 100, 10, 20, self.pipe.wallthickness + 1)
+        assert 'Defect depth ' in str(err.value)

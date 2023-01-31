d32b63241500a2d01d6c0c570c552f78040f4b55
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-01-31 17:39:40 +0400

    step

--- pipeline_danger/material.py
+++ pipeline_danger/material.py
@@ -7,7 +7,7 @@ class Material:
     def __init__(self, name, yield_strength):
         """New material."""
         self.name = name
-        self.yield_strength = yield_strength  # tekuchest
-        self.durability = None  # prochnost
-        self.toughness = None  # viazkost
+        self.yield_strength = yield_strength  # текучесть
+        self.durability = None  # прочность
+        self.toughness = None  # вязкость
         self.design_factor = 1  # from ASME B31.4, ASME B31.8 t or ASME B31.11

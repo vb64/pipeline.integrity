156018f7104d89710615d93049b6f385e7c5e4ce
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-01-31 17:33:50 +0400

    step

--- tests/test/__init__.py
+++ tests/test/__init__.py
@@ -9,5 +9,7 @@ class TestBase(TestCase):
         """Set up test data."""
         super().setUp()
         from pipeline_danger.material import Material
+        from pipeline_danger.pipe import Pipe
 
         self.material = Material("Steel", 20000)
+        self.pipe = Pipe(11200, 1420, 10, self.material)
--- tests/test/test_pipe.py
+++ tests/test/test_pipe.py
@@ -8,3 +8,11 @@ from . import TestBase
 
 class TestsPipe(TestBase):
     """Pipe class."""
+
+    def test_add_metal_loss(self):
+        """Method add_metal_loss."""
+        from pipeline_danger.defect import Type
+
+        defect = self.pipe.add_metal_loss(10, 100, 10, 20, 5)
+        assert len(self.pipe.metal_loss) == 1
+        assert defect.type == Type.MetalLoss

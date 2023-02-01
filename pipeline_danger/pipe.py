383afab3e3949f1386419ebd20be9b36f7f316d6
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-02-01 10:20:43 +0400

    refactor

--- pipeline_danger/pipe.py
+++ pipeline_danger/pipe.py
@@ -4,8 +4,20 @@ from . import Error as ErrorBase
 CIRCLE_MINUTES = 60.0
 
 
-class Error(ErrorBase):
-    """Pipe error."""
+class ErrDefectSize(ErrorBase):
+    """Wrong defect size (outside pipe)."""
+
+
+class ErrDefectDepth(ErrorBase):
+    """Wrong defect depth."""
+
+
+class ErrDefectOrientStart(ErrorBase):
+    """Wrong defect start orientdtion."""
+
+
+class ErrDefectOrientLength(ErrorBase):
+    """Wrong defect orientdtion length."""
 
 
 class Pipe:
@@ -24,24 +36,24 @@ class Pipe:
         from .defect.metal_loss import Item
 
         if (start + length) > self.length:
-            raise Error(
+            raise ErrDefectSize(
               "Defect border outside pipe: start {} + length {} = {} > pipe length {}.".format(
                 start, length, start + length, self.length
               )
             )
 
         if depth > self.wallthickness:
-            raise Error("Defect depth {} > pipe wall thickness {}.".format(depth, self.wallthickness))
+            raise ErrDefectDepth("Defect depth {} > pipe wall thickness {}.".format(depth, self.wallthickness))
 
         if not (-CIRCLE_MINUTES <= orient_start <= CIRCLE_MINUTES):
-            raise Error(
+            raise ErrDefectOrientStart(
               "Defect start orientdtion {} must be from {} to {} angle minutes.".format(
                 orient_start, -CIRCLE_MINUTES, CIRCLE_MINUTES
               )
             )
 
         if not (0 < orient_length <= CIRCLE_MINUTES):
-            raise Error(
+            raise ErrDefectOrientLength(
               "Defect orientdtion length {} must be from 0 to {} angle minutes.".format(
                 orient_length, CIRCLE_MINUTES
               )

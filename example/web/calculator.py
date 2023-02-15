1d775199332de2764eeeda401e15e2fa2e3080ef
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-02-15 15:45:50 +0400

    step

--- example/web/calculator.py
+++ example/web/calculator.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, g
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('main.html')
+    g.asme_url = url_for('asme')
+    return render_template('main.html', g=g)
+
+
+@app.route('/asme/', methods=['GET', 'POST'])
+def asme():
+    if request.method == 'POST':
+        return calc_asme()
+    return render_template('asme_input.html')
+
+
+def calc_asme():
+    g.asme_url = url_for('asme')
+    return render_template('asme_output.html', g=g)

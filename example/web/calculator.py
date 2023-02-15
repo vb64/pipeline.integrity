c728598de20f3457757eaa23dea249934f563237
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-02-15 16:22:34 +0400

    step

--- example/web/calculator.py
+++ example/web/calculator.py
@@ -1,6 +1,25 @@
 from flask import Flask, render_template, request, url_for, g
 
+from pipeline_integrity.material import Material
+from pipeline_integrity.pipe import Pipe
+from pipeline_integrity.method.asme_b31g import Context, State
+
+model = Context(Pipe(
+  11200,
+  1420,
+  16,
+  Material("Steel", 295),
+  7
+).add_metal_loss(
+  1000,
+  100,
+  10,
+  20,
+  1
+))
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def main():
@@ -10,9 +29,11 @@ def main():
 
 @app.route('/asme/', methods=['GET', 'POST'])
 def asme():
+    g.asme = model
     if request.method == 'POST':
         return calc_asme()
-    return render_template('asme_input.html')
+
+    return render_template('asme_input.html', g=g)
 
 
 def calc_asme():

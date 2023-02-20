6800f5ab56e262ea44602c9db542f388a2e46823
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-02-20 11:14:07 +0400

    step

--- example/web/gae/README.md
+++ example/web/gae/README.md
@@ -5,3 +5,4 @@ Version for GoogleAppEngine (Python3) platform.
 -   [Setting up](https://cloud.google.com/appengine/docs/standard/python3/runtime) GAE project.
 -   [Setting up](https://cloud.google.com/appengine/docs/standard/setting-up-environment?tab=python) development environment.
 -   [Memcache](https://cloud.google.com/appengine/docs/legacy/standard/python/memcache)
+-   [Migrating](https://cloud.google.com/appengine/docs/standard/python3/migrating-to-cloud-ndb) to [Cloud NDB](https://googleapis.dev/python/python-ndb/latest/index.html)
--- example/web/gae/source/main.py
+++ example/web/gae/source/main.py
@@ -1,11 +1,53 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, url_for, redirect, g
+from pipeline_integrity.material import Material
+from pipeline_integrity.pipe import Pipe
+from pipeline_integrity.method.asme_b31g import Context, State
+from i18n import activate
+
+model = Context(Pipe(11200, 1420, 16, Material("Steel", 295), 7
+).add_metal_loss(1000, 100, 10, 20, 1))
 
 app = Flask(__name__)
+lang_code = 'en'
+activate(app, lang_code)
 
 
 @app.route('/')
 def main():
+    g.asme_url = url_for('asme')
     return render_template('main.html')
+
+
+@app.route('/asme/', methods=['GET', 'POST'])
+def asme():
+    g.asme_url = url_for('asme')
+    if request.method == 'POST':
+        save_form(model.anomaly, request.form)
+        return redirect(g.asme_url)
+
+    state = model.pipe_state(is_explain=True)
+
+    g.result = _("No danger.")
+    if state == State.Replace:
+        g.result = _("Replacement of the pipe is necessary.")
+    elif state == State.Repair:
+        g.result = _("Repair or pressure reduction to {} required.").format(round(model.safe_pressure, 2))
+
+    g.explain = model.explain().replace('\n', '<br>')
+    g.asme = model
+
+    return render_template('asme.html', g=g)
+
+def save_form(defect, form):
+    pipe = defect.pipe
+    pipe.diameter = float(form['diameter'])
+    pipe.wallthickness = float(form['wall'])
+    pipe.material.yield_strength = float(form['smys'])
+    pipe.maop = float(form['pressure'])
+
+    defect.length = float(form['length'])
+    defect.depth = float(form['depth'])
 
 
 if __name__ == '__main__':

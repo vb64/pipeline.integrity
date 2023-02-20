8bf2a3d17f5f35db5f071a38fa496eb201f19d4a
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-02-20 11:46:14 +0400

    flask session

--- example/web/gae/source/main.py
+++ example/web/gae/source/main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, url_for, redirect, g
+from flask import Flask, render_template, request, url_for, redirect, session, g
 from pipeline_integrity.material import Material
 from pipeline_integrity.pipe import Pipe
 from pipeline_integrity.method.asme_b31g import Context, State
@@ -9,6 +9,7 @@ model = Context(Pipe(11200, 1420, 16, Material("Steel", 295), 7
 ).add_metal_loss(1000, 100, 10, 20, 1))
 
 app = Flask(__name__)
+app.secret_key = b'YourSecretKeyHere'
 lang_code = 'en'
 activate(app, lang_code)
 
@@ -21,6 +22,12 @@ def main():
 
 @app.route('/asme/', methods=['GET', 'POST'])
 def asme():
+    if 'session_id' in session:
+        print('session_id', session['session_id'])
+    else:
+        session['session_id'] = 'new_session'
+        print('NO session_id')
+
     g.asme_url = url_for('asme')
     if request.method == 'POST':
         save_form(model.anomaly, request.form)

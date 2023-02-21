0b4883cb274d1afb9b8897677d1cfa06846f4afa
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-02-21 10:22:14 +0400

    Use google-cloud-ndbm in GAE example (#18)

--- example/web/gae/source/main.py
+++ example/web/gae/source/main.py
@@ -1,23 +1,10 @@
-import os
 from flask import Flask, render_template, url_for, g
-from google.cloud import ndb
+from google_cloud_ndbm.flask import bind
 from i18n import activate, LANG_CODE
 from asme import asme_page
 
-# https://stackoverflow.com/questions/43628002/google-vision-api-project-not-passed
-os.environ["GCLOUD_PROJECT"] = "wot-online-hours"
-client = ndb.Client()
-
-def ndb_wsgi_middleware(wsgi_app):
-    def middleware(environ, start_response):
-        with client.context():
-            return wsgi_app(environ, start_response)
-
-    return middleware
-
-
 app = Flask(__name__)
-app.wsgi_app = ndb_wsgi_middleware(app.wsgi_app)  # Wrap the app in middleware.
+bind(app, "wot-online-hours")
 app.secret_key = b'YourSecretKeyHere'
 app.register_blueprint(asme_page)
 activate(app, LANG_CODE)

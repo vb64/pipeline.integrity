c2d916ba472f2652c1285cff55610288836b06d5
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-02-21 16:41:01 +0400

    step

--- example/web/gae/backend/main.py
+++ example/web/gae/backend/main.py
@@ -1,16 +1,28 @@
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
 from flask import Flask
+from google.cloud import ndb
 from google_cloud_ndbm.flask import bind
 
 app = Flask(__name__)
 bind(app, "wot-online-hours")
 
 
+class AsmeB31G(ndb.Model):
+    last_used = ndb.DateTimeProperty(indexed=True, auto_now=True)
+
+
 @app.route('/')
 def main():
-    logging.warning("### test5 Backend start: %s", str(datetime.utcnow()))
-    return 'test5 Backend OK'
+    border = datetime.utcnow() - timedelta(days=1)
+    query = AsmeB31G.query(AsmeB31G.last_used < border)
+    keys = query.fetch(100, keys_only=True)
+    if keys:
+        ndb.model.delete_multi(keys)
+
+    logging.warning("### Purge old AsmeB31G keys: %s", len(keys))
+
+    return 'AsmeB31G keys: {}'.format(len(keys))
 
 
 if __name__ == '__main__':

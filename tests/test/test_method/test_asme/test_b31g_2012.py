c6c5c53086a7ba901a0497758dc07b91f1a91dce
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-02-27 14:21:32 +0400

    step

--- tests/test/test_method/test_asme/test_b31g_2012.py
+++ tests/test/test_method/test_asme/test_b31g_2012.py
@@ -53,3 +53,30 @@ class TestsReadme2012(TestAsme):
         assert round(asme.erf(is_explain=True), 3) < 1
         # print('###')
         # print(asme.explain())
+
+
+class Tests2012(TestAsme):
+    """Class B31G_2012 methods."""
+
+    def setUp(self):
+        """Set up test data."""
+        super(Tests2012, self).setUp()
+
+        from pipeline_integrity.method.asme.b31g_2012 import Context
+
+        self.defect_en.pipe.material.smts = 1.5 * self.defect_en.pipe.material.smys
+        self.asme = Context(self.defect_en)
+
+    def test_s_flow(self):
+        """Method s_flow."""
+        pipe = self.asme.anomaly.pipe
+        pipe.material.smts = 1.01 * pipe.material.smys
+
+        assert self.asme.s_flow() == pipe.material.smts
+
+    def test_get_stress_fail_mod(self):
+        """Method get_stress_fail_mod."""
+        assert round(self.asme.z_param, 3) == 0.454
+        self.asme.anomaly.length = 50
+        assert round(self.asme.z_param, 3) == 70.862
+        assert round(self.asme.get_stress_fail_mod(), 3) == 54707.228
